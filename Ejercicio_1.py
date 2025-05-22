a3c5af464634a1c032275fc5d61e5f97f7c08a92
Jhonathan Muñoz <jhonathancamilomunoz706@gmail.com>
2025-05-22 09:32:43 -0500

    Update Ejercicio_1.py

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -27,8 +27,8 @@ def numero_mas_frecuente(lista):
         return numeros_mas_frecuentes[0]
 
 # Ejemplos de uso
-print(numero_mas_frecuente([0]))               # Debe devolver 0
-print(numero_mas_frecuente([-4, -4, -5, -5]))  # Debe devolver -5 (no -4 como decías en el comentario)
-print(numero_mas_frecuente([1, 2, 3, 4, 5]))   # Todos con frecuencia 1, debe devolver 1 (el menor)
-print(numero_mas_frecuente([2, 2, 1, 1, 3]))   # Empate entre 2 y 1, debe devolver 1
-print(numero_mas_frecuente([]))                # Imprime "arreglo vacío" y devuelve None
+print(numero_mas_frecuente([0]))               
+print(numero_mas_frecuente([-4, -4, -5, -5]))  
+print(numero_mas_frecuente([1, 2, 3, 4, 5]))   
+print(numero_mas_frecuente([2, 2, 1, 1, 3]))   
+print(numero_mas_frecuente([]))                
